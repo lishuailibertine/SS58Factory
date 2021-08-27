d744b6f862453382305bf73f9157c0ba9afbd837
lishuailibertine <lishuai19@yeah.net>
2021-08-27 11:57:21 +0800

    fix
    fix

--- Tests/SS58FactoryTests/SS58FactoryTests.swift
+++ Tests/SS58FactoryTests/SS58FactoryTests.swift
@@ -6,7 +6,7 @@ final class SS58FactoryTests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        
+        SS58AddressFactory()
     }
 
     static var allTests = [
