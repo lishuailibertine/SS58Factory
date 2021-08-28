b0dd2791b518ee98570642e328902ca3da3a62d5
lishuailibertine <lishuai19@yeah.net>
2021-08-28 18:53:23 +0800

    fix

--- Package.swift
+++ Package.swift
@@ -8,7 +8,7 @@ let package = Package(
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "SS58AddressFactory",
+            name: "SS58Factory",
             targets: ["SS58Factory"]),
         .library(name: "Blake2bFactory",
             targets: ["Blake2bFool"])
