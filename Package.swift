86a53fe096ccace058ff7fb80f8d5b4b3e7d4e2f
lishuailibertine <libertine9458@gmail.com>
2024-11-05 16:48:38 +0800

    fix

--- Package.swift
+++ Package.swift
@@ -11,23 +11,21 @@ let package = Package(
             name: "SS58Factory",
             targets: ["SS58Factory"]),
         .library(name: "Blake2bFactory",
-            targets: ["Blake2bFool"]),
-        .library(name: "SubstrateScrypt", targets: ["SubstrateScrypt"])
+            targets: ["Blake2bFool"])
     ],
     dependencies: [
-        .package(name: "scrypt", url: "https://github.com/v57/scrypt.c.git", from: "0.1.1"),
         .package(name: "Blake2", url: "https://github.com/lishuailibertine/Blake2.swift", from: "0.1.3"),
-        .package(name: "UncommonCrypto", url: "https://github.com/lishuailibertine/UncommonCrypto.swift", from: "0.1.4"),
+        .package(name: "UncommonCrypto", url: "https://github.com/lishuailibertine/UncommonCrypto.swift", from: "0.1.4")
+        
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "SS58Factory",
-            dependencies: ["Blake2bFool","SubstrateScrypt"]),
+            dependencies: ["Blake2bFool"]),
         .target(name: "Blake2bFool",
                 dependencies: ["Blake2","UncommonCrypto"]
-            ),
-        .target(name: "SubstrateScrypt",dependencies: ["scrypt"]),
+            )
     ]
 )
